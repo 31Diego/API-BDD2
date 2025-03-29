b5eb66a2bb86382ee36ecde275463e31589255f1
Bea <beatrizorozcodelgado@gmail.com>
2025-03-29 12:40:44 +0100

    Añado consulta productos

--- PHP/ManageDB.php
+++ PHP/ManageDB.php
@@ -3,14 +3,10 @@ include_once 'DB.php';
 class ManageBD extends DB{
   public function getQueries(){
 
-	
-	
-
+	$productos= $this->connect()->query('SELECT * FROM productos');
 
 	$queries = array (
-		
-		
-		
+		"productos"=>$productos
 	);
 	
 		return $queries;
