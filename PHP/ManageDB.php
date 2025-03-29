c20b82c21117c678f032337f76717ef6e94a5986
Maritoz <maritox47@gmail.com>
2025-03-29 19:55:44 +0100

    consulta empleados

--- PHP/ManageDB.php
+++ PHP/ManageDB.php
@@ -3,14 +3,11 @@ include_once 'DB.php';
 class ManageBD extends DB{
   public function getQueries(){
 
+	$empleados= $this->connect()->query('SELECT * FROM empleados');
 	
-	
-
 
 	$queries = array (
-		
-		
-		
+		"empleados"=>$empleados
 	);
 	
 		return $queries;
