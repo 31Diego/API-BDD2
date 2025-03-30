ed4db87884a1fd4d99b18714aeb3e79aa00a75ac
Diego Martinez <114482789+31Diego@users.noreply.github.com>
2025-03-30 13:26:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,121 @@
+
+import mongoose from 'mongoose';
+import { getdata } from './api.js';
+const { Schema, model } = mongoose;
+let uri = 'mongodb://localhost:27017/restaurante';
+//trayendo la data del api
+const query = await getdata().then(data => {
+    //console.log(data);
+    return data;
+}).catch(error => {
+    console.log('no va');
+    process.exit(0);
+});
+//console.log(query);
+// conectando a la bd
+const options = {
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect(uri, options).then(
+    () => {
+        console.log('se ha conectado exitosamente')
+    },
+    err => {
+        console.log('no se ha podido conectar');
+        process.exit(0);
+    });
+
+// definiendo schemas y modelos en mongoose
+
+
+<<<<<<< HEAD
+const clientesSchema = new Schema({
+    id: { type: Number, required: true, unique: true },
+    nombre: { type: String },
+    email: { type: String, unique: true },
+    direccion: { type: String },
+    fecha_registro: { type: Date, default: Date.now }, 
+    historial_pedidos: { type: Schema.Types.Mixed, default: [] } 
+    
+}, );
+
+const empleadosSchema = new Schema({
+    id: { type: Number, required: true, unique: true },
+    nombre: { type: String },
+    puesto: { type: String},
+    salario: { type: String },
+    contratado_el: { type: Date, default: Date.now },
+    
+});
+const pedidosSchema = new Schema({
+    id: { type: Number, required: true, unique: true },
+    id_cliente: { type: Number },
+    id_producto: { type: Number },
+    cantidad: { type: Number },
+    total: { type: Number },
+    fecha_pedido: { type: Date, default: Date.now },
+    estado: { 
+        type: String, 
+        enum: ['Pendiente', 'En preparación', 'Entregado', 'Cancelado'], 
+        default: 'Pendiente' 
+    },
+});
+const productosSchema = new Schema({
+    id: { type: Number, required: true, unique: true },
+    nombre: { type: String },
+    descripcion: { type: String },
+    precio: { type: Number },
+    stock: { type: Number },
+    categoria: { 
+        type: String, 
+        enum: ['Entrante', 'Plato Principal', 'Postre', 'bebida'], 
+        default: 'Entrante' 
+    },
+    disponible: {type: Number},
+    ingredientes: { type: Schema.Types.Mixed, default: [] }
+    
+});
+
+
+
+let clientes = new mongoose.model('clientes', clientesSchema);
+let empleados = new mongoose.model('empleados', empleadosSchema);
+let pedidos = new mongoose.model('pedidos', pedidosSchema);
+let productos = new mongoose.model('productos', productosSchema);
+=======
+
+
+
+
+
+
+>>>>>>> 8df51e6b0a7c3157ccbe7a8da09ff9ac6ec49e5d
+
+
+console.log(query.course);
+try {
+<<<<<<< HEAD
+   
+    let inserted_a = await clientes.insertMany(query.clientes);
+    let inserted_b = await empleados.insertMany(query.empleados);
+    let inserted_c = await pedidos.insertMany(query.pedidos);
+    let inserted_d = await productos.insertMany(query.productos);
+=======
+    
+
+   
+    
+>>>>>>> 8df51e6b0a7c3157ccbe7a8da09ff9ac6ec49e5d
+    //console.log(inserted_a);
+    process.exit(0);
+} catch (e) {
+    console.log('Some error');
+    console.log(e);
+    process.exit(0);
+}
+
